c61d07d2247d7c1dfaaecfb06008e307e4a7a9df
Sachin Gavali <sachingavali321@gmail.com>
2024-01-04 04:57:25 +0530

    react-router-set

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './root/index'
+import SigninForm from './auth/forms/SigninForm'
+import SignupForm from './auth/forms/SignupForm'
+import AuthLayout from './auth/AuthLayout'
+import RootLayout from './root/RootLayout'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+
+          {/*  public routes  */}
+          <Route element={<AuthLayout />}>
+            <Route path='/sign-in' element={<SigninForm />} />
+            <Route path='/sign-up' element={<SignupForm />} />
+          </Route>
+          {/* Private route  */}
+          {/*  index means index page home page  */}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
