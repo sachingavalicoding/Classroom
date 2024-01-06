03ecc26ce91bac8a580c802890e04c28595ac55e
Sachin Gavali <sachingavali321@gmail.com>
2024-01-06 07:56:07 +0530

    HOme component update

--- src/root/pages/Home.tsx
+++ src/root/pages/Home.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" w-full h-screen">
+      <div className="w-full text-red">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sint
+        molestias eum a sapiente labore magni explicabo quasi, minima aspernatur
+        recusandae facilis veritatis. Eaque, hic eos. Consectetur id minus
+        sapiente.
+      </div>
+    </div>
+  );
 };
 
 export default Home;
